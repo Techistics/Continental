8ecafbeb6057db6db082e6569bd3fb5a83724319
Techistics <lodhihasnain70@gmail.com>
2025-10-08 11:43:15 +0500

    debug

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -4,6 +4,7 @@ import pool from "../../../../db"; // adjust path
 export async function GET() {
   try {
     const { rows } = await pool.query("SELECT * FROM menu"); // table name
+    console.log(rows);
     return NextResponse.json(rows);
   } catch (error) {
     console.error("❌ API /menu error:", error);
